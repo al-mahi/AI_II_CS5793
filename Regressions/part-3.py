5ae8ee8252154bc9253720bcf9168631f4551f27
al-mahi <syed.al.mahi@gmail.com>
2016-10-06 12:54:24 -0500

    assn3

--- Regressions/part-3.py
+++ Regressions/part-3.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def PHI(x, m=0):
+def PHI_linear(x, m=0):
     """
     :type x: np.ndarray
     :param basis: polynomial or radial
@@ -20,7 +20,18 @@ def PHI(x, m=0):
     """
     return np.power(x, range(m))
 
+def PHI_radial(x, m=0):
+    """
+    :type x: np.ndarray
+    :rtype: np.ndarray
+    """
+    mu = np.linspace(x.min(), x.max(), m)
+    sigma = np.fabs(mu[0] - mu[1])
+    return np.exp(-(x - mu)**2 / 2.*sigma**2)
+
+
 if __name__=="__main__":
+    # ----------using radial basis--------------------------
     beta = .0025
     data = np.loadtxt("crash.txt")
     training = data[:-1:2]
@@ -42,17 +53,10 @@ if __name__=="__main__":
     L = 50
 
     for alpha in np.logspace(-8, 0, 100):
-        phi = PHI(x, L)
+        phi = PHI_linear(x, L)
         w = np.linalg.solve(phi.T.dot(phi) + (alpha/beta) * np.identity(n=L), phi.T.dot(t))
-        # E = .5 * np.linalg.norm(t - phi.dot(w))**2
-        # rms = np.sqrt(2. * E / N)
-        # RMS.append(rms)
-        # if rms < opt_rms:
-        #     opt_rms = rms
-        #     opt_m = L
-        #     opt_w = w
 
-        phi = PHI(x_test, L)
+        phi = PHI_linear(x_test, L)
         E_test = .5 * np.linalg.norm(t_test - phi.dot(w))**2
         rms = np.sqrt(2. * E_test / N)
         RMS_test.append(np.sqrt(2. * E_test / N))
@@ -77,14 +81,75 @@ if __name__=="__main__":
     tt = np.array(data[:, 1]).reshape(NN, 1)
 
     nx = np.linspace(start=0., stop=60., num=1000).reshape(1000, 1)
-    phi = PHI(nx, m=L)
+    phi = PHI_linear(nx, m=L)
     ny = phi.dot(test_opt_w)
 
     plt.plot(xx, tt, label="data")
     plt.plot(nx, ny, label="plot logspace")
     plt.xlabel("time")
     plt.ylabel("acceleration")
-    plt.title("best model on test on data alpha={:.6f}".format(test_opt_alpha))
+    plt.title("best model on test set\nusing linear basis alpha={:.6f}".format(test_opt_alpha))
+    plt.legend()
+    plt.show()
+
+    # ----------using radial basis--------------------------
+    beta = .0025
+    data = np.loadtxt("crash.txt")
+    training = data[:-1:2]
+    test = data[1::2]
+    N = training.shape[0]
+    x = np.array(training[:, 0]).reshape(N, 1)
+    t = np.array(training[:, 1]).reshape(N, 1)
+    x_test = np.array(test[:, 0]).reshape(N, 1)
+    t_test = np.array(test[:, 1]).reshape(N, 1)
+
+    opt_rms = np.inf
+    opt_w = None
+
+    test_opt_rms = np.inf
+    test_opt_alpha = None
+
+    RMS = []
+    RMS_test = []
+    L = 50
+
+    for alpha in np.logspace(-8, 0, 100):
+        phi = PHI_radial(x, L)
+        w = np.linalg.solve(phi.T.dot(phi) + (alpha/beta) * np.identity(n=L), phi.T.dot(t))
+
+        phi = PHI_radial(x_test, L)
+        E_test = .5 * np.linalg.norm(t_test - phi.dot(w))**2
+        rms = np.sqrt(2. * E_test / N)
+        RMS_test.append(np.sqrt(2. * E_test / N))
+        if rms < test_opt_rms:
+            test_opt_rms = rms
+            test_opt_w = w
+            test_opt_alpha = alpha
+
+    xmin = min(np.min(x), np.min(x_test))
+    xmax = max(np.max(x), np.max(x_test))
+    ymin = min(np.min(t), np.min(t_test))
+    ymax = max(np.max(t), np.max(t_test))
+    axes = plt.gca()
+    axes.set_xlim([xmin, xmax])
+    axes.set_ylim([ymin, ymax])
+    axes = plt.gca()
+    axes.set_xlim([xmin, xmax])
+    axes.set_ylim([ymin, ymax])
+
+    NN = data.shape[0]
+    xx = np.array(data[:, 0]).reshape(NN, 1)
+    tt = np.array(data[:, 1]).reshape(NN, 1)
+
+    nx = np.linspace(start=0., stop=60., num=1000).reshape(1000, 1)
+    phi = PHI_radial(nx, m=L)
+    ny = phi.dot(test_opt_w)
+
+    plt.plot(xx, tt, label="data")
+    plt.plot(nx, ny, label="plot logspace")
+    plt.xlabel("time")
+    plt.ylabel("acceleration")
+    plt.title("best model on test set\nusing radial basis alpha={:.6f}".format(test_opt_alpha))
     plt.legend()
     plt.show()
 
--- Regressions/part-4.py
+++ Regressions/part-4.py
@@ -4,11 +4,11 @@
 Author: S M Al Mahi
 CS5793: Artificial Intelligence II
 Assignment 3: Regressions
-Solution for Part 3 Linear Reg Baysian MAP
+Solution for Part 4 Linear Reg Bayesian MAP
 """
 from __future__ import print_function
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 
 def flower_to_float(s):
@@ -20,4 +20,85 @@ def flower_to_float(s):
     return d[s]
 
 
-irises = np.loadtxt('iris.data', delimiter=',', converters={4: flower_to_float})
+def PHI(x):
+    """
+    :type x: np.ndarray
+    :rtype: np.ndarray
+    """
+    m = x.shape[0]
+    return np.power(x, range(m))
+
+
+
+def f(w, x, t, alpha):
+    """
+    Bayesian MAP.
+    :param w: weights of radial basis function
+    :type w: np.ndarray
+    :param x: features
+    :type x: np.ndarray
+    :param t: labels
+    :type t: np.ndarray
+    :param alpha: precision param
+    :return: optimized weights
+    """
+    neg_log_prior = np.array((alpha/2.) * w.T.dot(w))
+    N = x.shape[0]
+    K = 3
+    neg_log_likeyhood= 0.
+    for n in range(N):
+        cost = 0.
+        for k in range(K):
+            print(w[k:k+5].T)
+            print(x[n])
+            print(PHI(x[n]))
+            cost += t[n, k] * w[k:k+5].T.dot(PHI(x[n]))
+        for l in range(K):
+            cost -= np.log(w[l:l+5].T.dot(PHI(x[n])))
+        neg_log_likeyhood += cost
+    return neg_log_prior - neg_log_likeyhood
+
+
+if __name__ == "__main__":
+    irises = np.loadtxt('iris.data', delimiter=',', converters={4: flower_to_float})
+    N = irises.shape[0]
+    K = 3
+    x = np.array(irises[:, :4])
+    x = np.hstack((np.ones(shape=(N, 1)), x))
+    y = np.zeros(shape=(N, K))
+    for i in range(N):
+        y[i, int(irises[i, 4])] = 1
+
+    # shuffle
+    ind = np.arange(N)
+    np.random.shuffle(ind)
+    data_x = np.array(x[ind.flatten()])
+    data_y = np.array(y[ind.flatten()])
+
+    x = data_x[:N/2]
+    t = data_y[:N/2]
+    x_test = data_x[N/2:]
+    t_test = data_y[N/2:]
+
+    alpha = 0.00215
+    w_init = np.ones(15)
+
+    # as minimization takes some time save the result and reuse
+    if False:
+        w_hat = minimize(f, w_init, args=(x, t, alpha)).x
+        np.save("opt_w_MAP_regression", w_hat)
+    else:
+        w_hat = np.load("opt_w_MAP_regression.npy")
+
+    t_hat = []
+    for i in range(x_test.shape[0]):
+        z = np.zeros(K)
+        for k in range(K):
+            z[k] = w_hat[k:k+5].T.dot(PHI(x_test[i]))
+        s = z / np.sum(z)
+        print(s, s.argmax())
+        t_hat.append(s.argmax())
+    y = np.array([c.argmax() for c in t_hat])
+
+    print("overall accuracy = {:.3%}".format(float(sum(t_hat == y)) / len(y)))
+
